c8bc2663794c824e23a6652f6529a916522cd408
Marlon Savian <marlon.savian@gmail.com>
2020-07-24 16:20:44 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import express from 'express'
 import { GraphQLServer } from 'graphql-yoga'
 
-const server = new GraphQLServer({
+// const server = new GraphQLServer({
 
-})
-
-server.start()
-
-// const app = express()
-
-// app.get('/', (request, response) => {
-//   return response.json({ message: 'Helloa world' })
 // })
 
-// app.listen(3333)
+// server.start()
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Helloa world' })
+})
+
+app.listen(3333)
